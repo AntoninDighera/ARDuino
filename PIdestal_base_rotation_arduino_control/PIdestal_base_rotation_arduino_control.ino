f4381771cd33ccbd19e1cc2f1f2d08f4aa17f632
Margaret Sy <margaretsy1016@gmail.com>
2018-05-26 12:20:49 -0700

    Replace PIdestal_base_rotation_arduino_control.ino with Dom's updates - motor moves more slowly - just release the motor to stop instead of sending stop signal

--- PIdestal_base_rotation_arduino_control/PIdestal_base_rotation_arduino_control.ino
+++ PIdestal_base_rotation_arduino_control/PIdestal_base_rotation_arduino_control.ino
@@ -1,15 +1,21 @@
 /* 
-This is the code for the pidestal, it uses the adafruit motorshield v2.3. I am currently revising this so be nice
+This is the code for the pidestal, it uses the adafruit motorshield v2.3.
 */
 #include <Adafruit_MotorShield.h>
 const int sens = 7;
 const int red = 11;  //assign red normally open button to pin 12
 const int blue = 12; //assign blue normally open button to pin 11
+int valr=0;
+int valb=0;
+int vals=0;
+int trg=0;
+int brun=1;
+int rrun=1;
 char * red_button = "red";
 char * blue_button = "blu";
 char * last_button;
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();   // Create the motor shield object with the default I2C address
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);         //Sets a motor at motor port 1
+Adafruit_DCMotor *myMotor = AFMS.getMotor(4);         //Sets a motor at motor port 1
 
 void setup() {
   Serial.begin(9600);  // set up Serial library at 9600 bps
@@ -26,33 +32,49 @@ void setup() {
  
 }
 
-const int MOTOR_SPEED = 50; //int value used to define motor speed, small impulses are aroun 5/10, maMOTOR_SPEED speed is 225 *personal note, 100 is eMOTOR_SPEEDtremely fast. 
+const int MOTOR_SPEED = 7; //int value used to define motor speed, small impulses are aroun 5/10, max speed is 225 *personal note, 100 is extremely fast. 
 
 void loop() {
+
+  valr=digitalRead(red);
+  valb=digitalRead(blue);
+  vals=digitalRead(sens);
   if(digitalRead(red) == LOW && digitalRead(sens)!=0){ // On red button press and if light sensor is blocked
     last_button = red_button;
-    myMotor->run(FORWARD); //runs your motor "forward", make sure you test what forward is so you dont destoy anything eMOTOR_SPEEDpensive
+    brun=1;
+    trg=0;
+    myMotor->run(FORWARD); //runs your motor "forward", make sure you test what forward is so you dont destoy anything expensive
     myMotor->setSpeed(MOTOR_SPEED);  //sets motor speed to constant integer value defined outside of loop 
   }
   else if(digitalRead(blue)== LOW && digitalRead(sens)!=0){ // On Blue button press and if light sensor is blocked
     last_button = blue_button;
+    rrun=1;
+    trg=0;
     myMotor->run(BACKWARD); //run the motor backwards 
     myMotor->setSpeed(MOTOR_SPEED);   //sets motor speed to constant integer value defined outside of loop
   }
   else if(digitalRead(sens)==0){
     if (last_button == red_button){
+      rrun=1;
+      trg=0;
       myMotor->run(BACKWARD); //run the motor backwards 
       myMotor->setSpeed(MOTOR_SPEED);   //sets motor speed to constant integer value defined outside of loop
     }
     else if (last_button == blue_button) {
+      brun=1;
+      trg=0;
       myMotor->run(FORWARD); //runs your motor "forward"
       myMotor->setSpeed(MOTOR_SPEED);  //sets motor speed to constant integer value defined outside of loop 
     }
   }
   else {
-    myMotor->setSpeed(0);
+    //myMotor->setSpeed(0);
+    myMotor->run(RELEASE);
   }    
   delay(10); //wait 10 miliseconds to restart this loop
+  //Serial.println(valr);
+  Serial.println(*last_button);
+  Serial.println(vals); 
 }
  
 
